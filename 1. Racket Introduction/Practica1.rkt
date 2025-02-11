a0bb8d8c5ef111bb060eaadff4e995c1ace88e17
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-02-11 16:38:16 -0600

    practica1 completada

--- 1. Racket Introduction/Practica1.rkt
+++ 1. Racket Introduction/Practica1.rkt
@@ -1,4 +1,4 @@
-#lang racket  ; Especifica el lenguaje Racket
+#lang plai ; Especifica el lenguaje Racket
 
 #|
 Team members:
@@ -102,64 +102,116 @@ Team members:
 
 
 ; 7. Una función recursiva que rota una lista hacia la izquierda un número de veces dado, recibe una lista lst y un número para rotar n, devuelve la lista ya rotada.
-
+(define (rotate-list lst n)
+  (cond
+    [(or (empty? lst) (zero? n)) lst]  ; Caso base: si la lista está vacía o n es 0, devolver la lista
+    [else
+     (rotate-list (append (rest lst) (list (first lst)))  ; Rotar la lista hacia la izquierda
+                  (sub1 n))]))  ; Disminuir n en 1
 
 
 ; 8. Una función recursiva recibe una lista de números lst y devuelve la suma de los elementos de una lista alternando entre suma y restar cada elemento.
-
+(define (zigzag-sum lst)
+  (define (helper lst sign)
+    (if (null? lst)
+        0
+        (+ (* sign (car lst)) (helper (cdr lst) (* sign -1)))))
+  (helper lst 1))
 
 
 ; 9. Una función recursiva recibe un número n y devuelve una lista de todas las combinaciones válidas de paréntesis para n pares.
-
+(define (generate-brackets n)
+  (define (helper open close current)
+    (cond
+      [(and (zero? open) (zero? close)) (list current)]
+      [(zero? open) (helper open (sub1 close) (string-append current ")"))]
+      [(= open close) (helper (sub1 open) close (string-append current "("))]
+      [else (append (helper (sub1 open) close (string-append current "("))
+                          (helper open (sub1 close) (string-append current ")")))]))
+  (helper n n ""))
 
 
 ; 10. Una función recursiva recibe un número entero n y devuelve el número de pasos necesarios para reducir el número dado a 1 siguiendo estas reglas:
 ; Si es par, divídelo en 2.
 ; Si es impar, multiplícalo por 3 y súmale 1.
-;
+
+(define (contar-pasos-uno n)
+  (cond
+    ((= n 1) 0)
+    ((even? n)
+     (+ 1 (contar-pasos-uno (/ n 2))))
+    (else        
+     (+ 1 (contar-pasos-uno (+ (* n 3) 1))))))
+
 
 ; Pruebas
-(displayln "Ejercicio 1")
+(displayln "----- Ejercicio 1 -----")
 (displayln (parentesis-balanceados? "((()"))
 (displayln (parentesis-balanceados? "(())"))
 (displayln (parentesis-balanceados? "()"))
 (displayln (parentesis-balanceados? ")("))
 
-(displayln "Ejercicio 2")
+(displayln "----- Ejercicio 2 -----")
 (displayln (vocal-predominante? "adgaaaasc"))
 (displayln (vocal-predominante? "adfttkyasc"))
 (displayln (vocal-predominante? "jskdlfnavcs"))
 (displayln (vocal-predominante? "aeioufj"))
 
-(displayln "Ejercicio 3")
+(displayln "----- Ejercicio 3 -----")
 (displayln (cadenas-isomorfas? "egg" "add"))
 (displayln (cadenas-isomorfas? "foo" "bar"))
 (displayln (cadenas-isomorfas? "paper" "title"))
 (displayln (cadenas-isomorfas? "hi" "hello"))
 
-
-(displayln "Ejercicio 4")
+(displayln "----- Ejercicio 4 -----")
 (displayln (automorfico? 5))
 (displayln (automorfico? 6))
 (displayln (automorfico? 23))
 (displayln (automorfico? 9))
 
-(displayln "Ejercicio 5")
+(displayln "----- Ejercicio 5 -----")
 (displayln (clasificar-numeros '(5 -3 0 12 -1 8 0)))
 (displayln (clasificar-numeros '(23 -45 0 0 -1 8 0)))
 (displayln (clasificar-numeros '(5 -3 0 2 1 1 0)))
 (displayln (clasificar-numeros '(0 0 0 0 0 0 0)))
 
-(displayln "Ejercicio 6")
+(displayln "----- Ejercicio 6 -----")
 (displayln (lanzar-dado 5))
 (displayln (lanzar-dado 3))
 (displayln (lanzar-dado 1))
 (displayln (lanzar-dado 6))
 
-(displayln "Ejercicio 7")
+(displayln "----- Ejercicio 7 -----")
+(test (rotate-list '(1 2 3 4 5) 2)
+      '(3 4 5 1 2))
+(test (rotate-list '(a b c d) 3)
+      '(d a b c))
+(test (rotate-list '() 5)
+      '())
+(test (rotate-list '(1 2 3) 5)
+      '(3 1 2))
 
-(displayln "Ejercicio 8")
+(displayln "----- Ejercicio 8 -----")
+(test (zigzag-sum '(1 2 3 4 5))  ; Lista: (1 2 3 4 5)
+      3)                         ; Resultado esperado: 1 - 2 + 3 - 4 + 5 = 3
+(test (zigzag-sum '(5))          ; Lista: (5)
+      5)                         ; Resultado esperado: 5
+(test (zigzag-sum '())           ; Lista vacía
+      0)                         ; Resultado esperado: 0
 
-(displayln "Ejercicio 9")
+(displayln "----- Ejercicio 9 -----")
+(test (generate-brackets 0)
+      '(""))
+(test (generate-brackets 2)
+      '("(())" "()()"))
+(test (generate-brackets 3)
+      '("((()))" "(()())" "(())()" "()(())" "()()()"))
 
-(displayln "Ejercicio 10")
+
+(displayln "----- Ejercicio 10 -----")
+(test (contar-pasos-uno 6)
+      8)
+(test (contar-pasos-uno 15)
+      17)
+(test (contar-pasos-uno 1)
+      0)
