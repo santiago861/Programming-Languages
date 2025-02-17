c1286d954df0eb89c54f5fe90ee9e56e6d76c894
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-02-16 18:57:52 -0600

    exercise 2 was added

---
+++ 2. Funciones de orden superior y tipos de datos abstractos/Practica2.rkt
@@ -0,0 +1,18 @@
+#lang plai
+
+#|
+Team members:
+- Reyes Medina Santiago Iván - 320308598
+- López Espinoza Ashley Yael - 319129391
+|#
+
+
+; my-map, my-foldl, my-foldr, my-filter, my-take, my-drop -> utilizar implementaciones vistas en clase
+; En caso de necesitar alguna función auxiliar ésta deberá ser anónima
+
+
+
+; 1. Una función que recibe una lista de números enteros lst y devuelve una lista de la representación binaria de los números de lst
+
+
+; 2. Una función que recibe una lista de números enteros lst y regresa una lista con los números que son palindromos en lst.
